798ecfe4f93974a1a35d11f83a5b49eeb8da5e89
DifolderyXXL <87066000+DifolderyXXL@users.noreply.github.com>
2023-07-07 19:44:14 +0300

    plugins

--- DiSi.Tests.PluginsEngine/Program.cs
+++ DiSi.Tests.PluginsEngine/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using DiSi.PluginsEngine;
+
+var plugin = PluginLoader.LoadNew(@"C:\C#\Github\DiSi.PluginsEngine\DiSi.Tests.TestPlugin\bin\Debug\net7.0\DiSi.Tests.TestPlugin.dll");
+plugin.Execute();
