3d1b1f4cee754839b875c7fd59f8cbeed6054742
Александр Лычкин <Alexander-Si.World@yandex.ru>
2023-07-07 19:19:07 +0300

    add ass

---
+++ DiSi.PluginsEngine/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiSi.PluginsEngine;
+public class Plugin
+{
+    public Plugin(Assembly assembly)
+    {
+        Assembly = assembly;
+    }
+
+    internal Assembly Assembly { get; set; }
+}
--- DiSi.PluginsEngine/PluginLoader.cs
+++ DiSi.PluginsEngine/PluginLoader.cs
@@ -1,7 +1,16 @@
-﻿namespace DiSi.PluginsEngine
+﻿using System.Reflection;
+
+namespace DiSi.PluginsEngine
 {
     public class PluginLoader
     {
-
+        public static Plugin LoadNew(string path)
+        {
+            if(File.Exists(path))
+            {
+                var assembly = Assembly.LoadFrom(path);
+                return new Plugin(assembly);
+            }
+        }
     }
 }
